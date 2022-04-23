a385d32b83864c26d0a80e5e6d9f0e7f2fb1edae
ms0503 <ms0503@outlook.com>
2022-04-23 20:48:57 +0900

    色々。

---
+++ essential.js
@@ -0,0 +1,24 @@
+($ => {
+    function removeMenu(shade) {
+        shade.remove();
+        $('.nav-container').slideUp('fast');
+    }
+    $.ajax({
+        dataType: 'html',
+        url: 'https://ms0503.github.io/kosyukai2021/navigation.html'
+    }).done(data => $('.nav-container').html(data));
+    $.ajax({
+        dataType: 'html',
+        url: 'https://ms0503.github.io/kosyukai2021/footer.html'
+    }).done(data => $('.footer-contents').html(data));
+    $('.navigation-button').on('click', e => {
+        e.preventDefault();
+        let shade = $('.navigation-effect')[0];
+        if(shade === undefined) {
+            $('<div>').addClass('navigation-effect').on('click', () => {
+                removeMenu(this);
+            }).appendTo($('body'));
+            $('.nav-container').slideDown('fast');
+        } else removeMenu(shade);
+    });
+})(jQuery);
