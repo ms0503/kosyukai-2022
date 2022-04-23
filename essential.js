689e20a6dcdfb4db1473d36535f45efedb8d7386
ms0503 <ms0503@outlook.com>
2022-04-23 21:09:20 +0900

    微修正。

--- essential.js
+++ essential.js
@@ -1,24 +1,22 @@
-($ => {
-    function removeMenu(shade) {
-        shade.remove();
-        $('.nav-container').slideUp('fast');
-    }
-    $.ajax({
-        dataType: 'html',
-        url: 'https://ms0503.github.io/kosyukai2021/navigation.html'
-    }).done(data => $('.nav-container').html(data));
-    $.ajax({
-        dataType: 'html',
-        url: 'https://ms0503.github.io/kosyukai2021/footer.html'
-    }).done(data => $('.footer-contents').html(data));
-    $('.navigation-button').on('click', e => {
-        e.preventDefault();
-        let shade = $('.navigation-effect')[0];
-        if(shade === undefined) {
-            $('<div>').addClass('navigation-effect').on('click', () => {
-                removeMenu(this);
-            }).appendTo($('body'));
-            $('.navigation').slideDown('fast');
-        } else removeMenu(shade);
-    });
-})(jQuery);
+function removeMenu(shade) {
+    shade.remove();
+    $('.nav-container').slideUp('fast');
+}
+$.ajax({
+    dataType: 'html',
+    url: 'https://ms0503.github.io/kosyukai2021/navigation.html'
+}).done(data => $('.nav-container').html(data));
+$.ajax({
+    dataType: 'html',
+    url: 'https://ms0503.github.io/kosyukai2021/footer.html'
+}).done(data => $('.footer-contents').html(data));
+$('.navigation-button').on('click', e => {
+    e.preventDefault();
+    let shade = $('.navigation-effect')[0];
+    if(shade === undefined) {
+        $('<div>').addClass('navigation-effect').on('click', () => {
+            removeMenu(this);
+        }).appendTo($('body'));
+        $('.navigation').slideDown('fast');
+    } else removeMenu(shade);
+});
