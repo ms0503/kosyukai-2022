853d6b192e49de15d0a452a5a5b024dc32eba740
ms0503 <ms0503@outlook.com>
2022-04-27 17:29:34 +0900

    CSSファイルの読み込み場所を修正。

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 'use strict';
 
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
-import styles from '../styles/MyDocument.module.scss';
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
@@ -11,7 +10,7 @@ export default class MyDocument extends Document {
 
     render() {
         return (
-            <Html className={styles['html']} lang="ja">
+            <Html lang="ja">
                 <Head>
                     <meta charSet="UTF-8" />
                     <meta http-equiv="access-control-allow-origin" content="*" />
@@ -70,7 +69,7 @@ export default class MyDocument extends Document {
                     <link rel="manifest" type="application/json" href="manifest.json" />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?f=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
                 </Head>
-                <body className={styles['body']}>
+                <body>
                     <Main />
                     <NextScript />
                 </body>
